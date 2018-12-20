31f5bde03463c6af37fc73ce621db20b06a66ba9
Norm <owner0220@users.noreply.github.com>
2018-12-21 00:32:06 +0900

    Delete module_os_01.py

--- Python/module_os_01.py
+++
@@ -1,9 +0,0 @@
-#파일 이름 변경하기
-#OS==============================================================
-import os
-
-os.chdir(r'C:\Users\student\Downloads\work\SSAFY지원자')
-
-for filename in os.listdir("."):
-    os.rename(filename,"SSAFY_"+filename)
-
