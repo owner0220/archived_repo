d720dd82178520b6d8b8a44a975621717756bce3
norm <owner0220@gmail.com>
2019-04-08 04:53:17 +0000

    fix a tag

--- posts/views.py
+++ posts/views.py
@@ -17,12 +17,22 @@ def create(request):
             form.save()
             return redirect("posts:list")
         else:
-            
             pass
-
     else:
         #2. Postform을 인스턴스화 시켜서 form에 저장
         form = Postform()
     #3.form을 담아서 create.html을 보내준다.    
-    return render(request,'posts/create.html',{'form':form})
+    return render(request,'posts/form.html',{'form':form})
+    
+    
+def update(request,id):
+    post = Post.objects.get(id=id)
+    if request.method == "POST":
+        form = Postform(request.POST,instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect("posts:list")
+    else:
+        form = Postform(instance=post)
+    return render(request,'posts/form.html',{'form':form})
     
