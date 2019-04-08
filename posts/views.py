258d699f3c2316d4023b246eba3f765efcba1415
norm <owner0220@gmail.com>
2019-04-08 02:49:34 +0000

    create logic model,modelform

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import Postform
 
 # Create your views here.
 def list(request):
@@ -6,4 +7,19 @@ def list(request):
     
     
 def create(request):
-    pass
+    #1. get 방식으로 데이터를 입력할 form요청
+    if request.method == "POST":
+        form = Postform(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("posts:list")
+        else:
+            
+            pass
+
+    else:
+        #2. Postform을 인스턴스화 시켜서 form에 저장
+        form = Postform()
+    #3.form을 담아서 create.html을 보내준다.    
+    return render(request,'posts/create.html',{'form':form})
+    
