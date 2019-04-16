39dccee359d703b0f6f232d370458300c90b35aa
norm <owner0220@gmail.com>
2019-04-16 13:49:56 +0900

    190416

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
-from .forms import CustomUserCreationForm
 from django.contrib.auth import get_user_model
+from django.shortcuts import render,redirect
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 # Create your views here.
 def signup(request):
@@ -34,7 +35,8 @@ def user_page(request,id):
     User = get_user_model()
     user_info = User.objects.get(id=id)
     return render(request, "accounts/user_page.html", {'user_info':user_info})
-    
+
+@login_required    
 def follow(request,id):
     User = get_user_model()
     me = request.user
@@ -47,9 +49,23 @@ def follow(request,id):
             me.followings.add(you)
     
     return redirect('accounts:user_page', id)
+
+@login_required    
+def edit_profile(request, id):
+    User = get_user_model()
+    user = User.objects.get(id=id)
+    me = request.user
+    if me == user:
+        if request.method=="POST":
+            form = CustomUserChangeForm(request.POST, request.FILES, instance=user)
+            if form.is_valid():
+                form.save()
+                return redirect('accounts:user_page', id)
+        else:
+            form = CustomUserChangeForm(instance=user)
+        return render(request, 'accounts/form.html', {'form':form})
     
-    
-    
+    return redirect("posts:list")
     
     
     
